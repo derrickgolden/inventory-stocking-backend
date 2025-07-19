303b49de9266bf6550392891eb6f6069fc72e188
unknown <goldenderrick95@gmail.com>
2025-07-19 14:31:47 +0300

    handle from backend

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->file(public_path('index.html'));
+});
+
+Route::fallback(function () {
+    $index = public_path('index.html');
+
+    if (File::exists($index)) {
+        return response()->file($index);
+    }
+
+    abort(404);
+});
 
 // Route::get('/', function () {
 //     return file_get_contents(public_path('dist/index.html'));
